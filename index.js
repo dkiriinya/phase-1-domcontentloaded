4a8d2290946ac56ab19138f52570423fc59c0b35
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-03 11:53:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    let paragraphContent = document.getElementById('text')
+    if (paragraphContent){
+        paragraphContent.textContent = "This is really cool!"
+    }
+
+})
